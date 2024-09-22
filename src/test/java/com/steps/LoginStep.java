4c69b88e4b6709359c028d63e476c81a503dce9b
merhh <merhh@192.168.0.32>
2024-09-21 18:25:53 -0700

    new code added

--- src/test/java/com/steps/LoginStep.java
+++ src/test/java/com/steps/LoginStep.java
@@ -25,6 +25,7 @@ public class LoginStep {
 	@When("User enters username {string}")
 	public void user_enters_username(String username) {
 	 System.out.println("Entering username");
+	 System.out.println("TESTTT");
 	 }
 	
 	@When("User enters password {string}")
